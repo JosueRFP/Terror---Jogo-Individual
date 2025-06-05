3d66f2b1ea84b93b666fbe16860803e3b4cda72a
JosueRFP <josue.petri.senac@gmail.com>
2025-06-05 14:37:35 -0300

    Piscada do player

--- Terror Individual/Assets/Scripts/Player/PlayerView.cs
+++ Terror Individual/Assets/Scripts/Player/PlayerView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public enum PlayerSaw
@@ -7,6 +8,7 @@ public enum PlayerSaw
 
 public class PlayerView : MonoBehaviour, IInteraction
 {
+    [SerializeField] float blinkColdown;
     [SerializeField] float distance;
     Transform rayCastOrigin;
     PlayerSaw saw;
@@ -15,20 +17,49 @@ public class PlayerView : MonoBehaviour, IInteraction
 
     void Start()
     {
+        PlayerEyes(PlayerSaw.OpenEyes);
         Debug.DrawRay(rayCastOrigin.position, rayCastOrigin.forward * 10, Color.cyan);
     }
 
 
     private void Update()
-    {
-        if (Input.GetButtonDown("Fire1") && rayCastOrigin != null)
+    {        
+        if (rayCastOrigin != null)
         {
-            
+            Interact();
         }
+        else { }
     }
     public void Interact()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gameObject.SetActive(true);
 
+        }
+    }
+
+    private  void PlayerEyes(PlayerSaw vison)
+    {
+        switch (vison)
+        {
+            case PlayerSaw.OpenEyes:
+                break;
+            case PlayerSaw.Blink:
+                StartCoroutine(BlinkTime());
+                print("Piscou");
+                break;
+        }
+    }
+
+    IEnumerator BlinkTime()
+    {
+        blinkColdown++;
+        if (blinkColdown == 7)
+        {
+            blinkColdown = 0;
+            yield return new WaitForSeconds(blinkColdown);
+        }
     }
 
 }
